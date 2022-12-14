02f8e1c6abed738d9b247dca441ac0b4aa4479f7
jhoisz <jvrda@ufpi.edu.br>
2022-12-14 18:19:05 -0300

    Variáveis

---
+++ var_let_const.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  String apelido = "Jhois";
+  String nome = "Jhoisnáyra Vitória Rodrigues de Ameida";
+  int idade = 22;
+  double altura = 1.69;
+  bool geek = true;
+
+  List<dynamic> jhois = [altura, idade, geek, nome, apelido];
+}
---
+++ variaveis.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  String apelido = "Jhois";
+  String nome = "Jhoisnáyra Vitória Rodrigues de Ameida";
+  int idade = 22;
+  double altura = 1.69;
+  bool geek = true;
+
+  print("Olá, eu sou $apelido, \n"
+      "mas meu nome completo é $nome,\n"
+      "eu me considero geek? $geek.\n"
+      "Minha idade é $idade e\n"
+      "tenho $altura de altura.\n");
+}
